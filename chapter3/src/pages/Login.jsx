ef16ed1ef0efa18dcbf510219f5752c9b98419ae
yelynnn <lynn1833@ewhain.net>
2024-11-04 19:29:29 +0900

    feat: useForm Hooks 실습

---
+++ chapter3/src/pages/Login.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import useForm from '../hooks/use-form';
+import { ValidateLogin } from '../utils/validate';
+
+function Login() {
+  const login = useForm({
+    initialValue: {
+      email: '',
+      password: '',
+    },
+    validate: ValidateLogin,
+  });
+
+  console.log(login.values, login.errors, login.touched);
+
+  const handlePressLogin = () => {
+    console.log(login.values.email, login.values.password);
+  };
+
+  return (
+    <Logins>
+      <Title>로그인</Title>
+
+      <Input
+        error={login.touched.email && login.errors.email}
+        placeholder={'이메일을 입력해주세요!'}
+        type={'email'}
+        {...login.getTextInputProps('email')}
+      />
+      {login.touched.email && login.errors.email && (
+        <Error>{login.errors.email}</Error>
+      )}
+      <Input
+        error={login.touched.password && login.errors.password}
+        placeholder={'비밀번호를 입력해주세요!'}
+        type={'password'}
+        {...login.getTextInputProps('password')}
+      />
+      {login.touched.password && login.errors.password && (
+        <Error>{login.errors.password}</Error>
+      )}
+
+      <LoginButton onClick={handlePressLogin}>로그인</LoginButton>
+    </Logins>
+  );
+}
+
+const LoginButton = styled.button`
+  color: white;
+  background-color: #ee51b2;
+  border-radius: 7px;
+  width: 260px;
+  height: 30px;
+  margin-top: 5px;
+  &:disabled {
+    background-color: gray;
+  }
+`;
+
+const Title = styled.h2`
+  color: white;
+`;
+
+const Logins = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+  row-gap: 5px;
+`;
+
+const Input = styled.input`
+  width: 250px;
+  height: 25px;
+  border-radius: 7px;
+
+  /* 에러일 경우 border 색상 변경 */
+  border: ${(props) => (props.error ? '2px solid red' : '1px solid #ccc')};
+`;
+
+const Error = styled.div`
+  color: red;
+  font-size: 10px;
+  text-align: left;
+`;
+
+export default Login;
