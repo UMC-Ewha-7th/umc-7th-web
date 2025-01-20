1c4ef447f3cc6d5769155724dcb7a713b46d4826
devhaeun <lala819010@gmail.com>
2025-01-20 15:58:41 +0900

    style: 헤더 네비게이션 메뉴 생성 - RootLayout.tsx: 스타일 추가 및 공유되는 헤더와 Outlet 요소 배치 - Navbar: 네비게이션 메뉴 스타일 추가 및 페이지 이동

--- week10/yongcha-ts/src/layout/RootLayout.tsx
+++ week10/yongcha-ts/src/layout/RootLayout.tsx
@@ -1,12 +1,64 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import styled from "styled-components";
 
 const RootLayout = () => {
+    const isHome = location.pathname === '/';
     return (
-        <>
-            <div>YONGCHA</div>
-            <Outlet />
-        </>
+        <ScrollDiv>
+            <RootContainer>
+                <FixedHeader>
+                    <Navbar />
+                </FixedHeader>
+                <MainContainer padding={isHome.toString()}>
+                    <Outlet />
+                </MainContainer>
+            </RootContainer>
+        </ScrollDiv>
     )
 }
 
 export default RootLayout;
+
+const MainContainer = styled.div<{ padding: string }>`
+    ${({padding}) => {
+        const paddingLR = padding==='true' ? '10px' : '40px';
+        return `padding: 0-px ${paddingLR} 20px ${paddingLR};`
+    }}
+    background-color: black;
+    height: auto;
+    min-height: 100%;
+    position: relative;
+    min-width: 900px;
+    display: flex;
+    flex-direction: column;
+`
+
+const FixedHeader = styled.div`
+    position: fixed;
+    z-index: 11;
+    width: 100%;
+`
+
+const RootContainer = styled.div`
+    height: 100vh;
+    min-width: 1100px;
+`
+
+const ScrollDiv = styled.div`
+    overflow-y: auto;
+    &::-webkit-scrollbar {
+        width: 8px;
+        height: 8px;
+        border-radius: 6px;
+    }
+    
+    &::-webkit-scrollbar-track {
+        background: rgba(0, 0, 0, 0.4)
+    }
+
+    &::-webkit-scrollbar-thumb {
+        background: rgba(0, 0, 0, 0.3);
+        border-radius: 6px;
+    }
+`;
