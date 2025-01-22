5aa992c3970d216616908061f08b352804c43e22
devhaeun <lala819010@gmail.com>
2025-01-22 11:06:50 +0900

    Feat: 로그인 페이지 - username/password input-label 생성 - form 태그 이용: onSubmit 함수 연결 - onSubmit 함수에서 getAuth를 이용해 로그인 정보 일치하는지 확인 및 처리

--- week10/protected-todo/src/pages/LoginPage.tsx
+++ week10/protected-todo/src/pages/LoginPage.tsx
@@ -1,6 +1,33 @@
+import { FormEvent } from "react";
+import { getAuth } from "../apis/auth";
+
 const LoginPage = () => {
+    const handleLogin = async (e:FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const formData = Object.fromEntries(new FormData(e.currentTarget));
+        try {
+            const user = await getAuth({
+                username: String(formData.username),
+                password: String(formData.password)
+            });
+        } catch (e) {
+            alert('로그인 실패');
+            console.error(e);
+        };
+    }
     return (
-        <div>로그인</div>
+        <form onSubmit={handleLogin}>
+            <div>
+                <label htmlFor="username">username</label>
+                <input type="text" id="username" name="username" required />
+            </div>
+            <div>
+                <label htmlFor="password">password</label>
+                <input type="password" id="password" name="password" required />
+            </div>
+            <button type="submit">입력</button>
+        </form>
     )
 };
 
